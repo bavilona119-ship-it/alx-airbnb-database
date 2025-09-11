95393b52a0bf169e3991396530fcb06e4c13601b
bavilona119-ship-it <bavilona119@gmail.com>
2025-09-11 16:48:36 +0200

    Create partitioning.sql

---
+++ database-adv-script/partitioning.sql
@@ -0,0 +1,44 @@
+-- =====================================
+-- Initial Query: Retrieve all bookings with user, property, and payment details
+-- =====================================
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_method
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+
+-- =====================================
+-- Optimized Query
+-- Optimization: 
+--   1. SELECT only needed columns
+--   2. Use proper indexes (user_id, property_id, booking_id)
+--   3. Keep LEFT JOIN only where optional (payments)
+-- =====================================
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    pay.amount
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
